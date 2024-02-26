ac9a8da95f779f247fd3f079ea2ad9b6f3874a19
Eric Liu <liue@duq.edu>
2024-02-25 23:33:05 -0500

    Linked to placeholder API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import "./App.css"
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [robotArray, setRobotArray] = useState(robots);
+  const [robots, setRobots] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        setRobots(data);
+      })
+      .catch(error => console.error(error.message));
+  }, []);
 
   function onSearchChange(event) {
-    setRobotArray(robots.filter(robots => robots.name.toLowerCase().includes(event.target.value)));
+    setRobots(robots.filter(robots => robots.name.toLowerCase().includes(event.target.value)));
   }
 
+  if (robots.length === 0)
+    return <h1>Loading...</h1>;
   return (
     <div className="tc">
       <h1 id="title">RoboFriends</h1>
       <SearchBox onSearchChange={onSearchChange}/>
-      <CardList robots={robotArray}/>
+      <CardList robots={robots}/>
     </div>
   );
 }
